7e6bf3247f3eb62ed697614430bb3d7f3f8d8879
vutran <vutran1412@icloud.com>
2018-08-30 19:26:00 -0500

    Initial commit

---
+++ camelCase.py
@@ -0,0 +1,32 @@
+# Camel Casing Program
+# Author: Vu Tran
+
+# Function to turn a sentence user entered into a camel cased sentence
+def camel_case(user_string):
+    # Initialize a new list
+    new_list = []
+    # Initialize a new string
+    new_sentence = ""
+    # Splits the user sentence and saves the individual words into a list
+    string_list = user_string.split()
+    # Loop over the string_list and capitalize the entire word of every odd index
+    for i in range(len(string_list)):
+        # If index is even push everything to lower case
+        if i % 2 == 0:
+            # Append the newly lowered or all capped words into a list
+            new_list.append(string_list[i].lower())
+        # Else make everything upper case
+        else:
+            new_list.append(string_list[i].upper())
+    # Iterate through the list and concatenate all the words to the new sentence
+    for word in new_list:
+        new_sentence += word
+    # Return the new sentence
+    return new_sentence
+
+# Asks user to input a sentence
+user_input = input("Enter a sentence ")
+# Function call and save to a new string
+joined_string = camel_case(user_input)
+# Output
+print(joined_string)
---
+++ number_guessing.py
@@ -0,0 +1,29 @@
+# Number Guessing game
+# Author: Vu Tran
+
+# import random
+import random
+
+# function to guess random number
+def guess_number():
+    # generates a random number between 0 and 10 inclusive
+    random_number = random.randint(0, 11)
+    return random_number
+
+# Loop to keep the game going as long as player wants to play
+while True:
+    # Prompts user input
+    user_guess = int(input("Guess a number between 0 and 11"))
+    # Calls the function
+    random_number = guess_number()
+    # If and elif statements to check to see if user guessed correctly
+    if user_guess > random_number:
+        print("Too high, the number is {}".format(random_number))
+    elif user_guess < random_number:
+        print("Too low, the number is {}".format(random_number))
+    elif user_guess == random_number:
+        print("correct!")
+    # Gives user option to break out of the loop
+    try_again = input("Do you wanna play again?")
+    if try_again != "yes" or try_again != "yes":
+        break
