3550997c31178a8ab82f8d3641880739197ef3c0
Dreyke <dreykeb@gmail.com>
2018-09-06 19:31:41 -0500

    Added exception handling

--- guess_the_number.py
+++ guess_the_number.py
@@ -36,12 +36,15 @@ def main():
     secret = generate_secret(low, high)
 
     while True:
-        guess = get_guess()
-        result = check_guess(guess, secret)
-        print(result)
+        try:
+            guess = get_guess()
+            result = check_guess(guess, secret)
+            print(result)
 
-        if result == correct:
-            break
+            if result == correct:
+                break
+        except ValueError:
+            print("Error. Please enter a number.")
 
 
 if __name__ == '__main__':
